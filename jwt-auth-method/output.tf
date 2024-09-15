41b5a169ba7701336e98ece83bb48dd72c828449
vagrant <vagrant@hellocloud-native-box>
2024-09-15 09:40:17 +0000

    add output files

--- jwt-auth-method/output.tf
+++ jwt-auth-method/output.tf
@@ -1,4 +1,7 @@
 output "role-name" {
-
 value = vault_jwt_auth_backend_role.admin-role.role_name
+}
+output "openid_claims" {
+  description = "OpenID Claims for trust relationship"
+  value       = vault_jwt_auth_backend_role.admin-role.bound_claims
 }
