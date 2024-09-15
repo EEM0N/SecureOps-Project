1478ab98238350d93422d9c5e1fe3f0cc2c87072
vagrant <vagrant@hellocloud-native-box>
2024-09-15 02:09:39 +0000

    add files

---
+++ vault-cluster/main.tf
@@ -0,0 +1,13 @@
+resource "hcp_hvn" "vault_hvn" {
+  hvn_id         = "cohort7"
+  cloud_provider = "aws"
+  region         = var.aws_region
+  cidr_block     = "172.25.16.0/20"
+}
+
+resource "hcp_vault_cluster" "vault_cluster" {
+  cluster_id = "cohort7-vault-cluster"
+  hvn_id     = hcp_hvn.vault_hvn.hvn_id
+  tier       = "starter_small"
+  public_endpoint = true
+}
