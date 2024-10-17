1acbf225acc8300ff7c504096d3d592735dde4ac
vagrant <vagrant@hellocloud-native-box>
2024-10-17 12:19:40 +0000

    update day3 vpc

--- create-vpc-day3/create-env-workspace/variables.tf
+++ create-vpc-day3/create-env-workspace/variables.tf
@@ -11,7 +11,7 @@ variable "org_name" {
 variable "vault_url" {
   description = "The address of the Vault instance runs will access."
   type = string
-  default = "https://vault-cluster-id-public-vault-2a5d42b2.18e736ba.z1.hashicorp.cloud:8200" ### have to change new vault cluster id
+  default = "https://vault-cluster-id-public-vault-d5aeeffd.28871b91.z1.hashicorp.cloud:8200" ### have to change new vault cluster id
 }
 variable "run_role" {
   description = "TFC_VAULT_RUN_ROLE"
